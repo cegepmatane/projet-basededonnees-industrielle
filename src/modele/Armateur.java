3a9ed87bc1e721c8b74dc281c0ee5ebf566f8101
OlivierBeaupre <olivierbeaupre37@gmail.com>
2017-09-07 15:45:49 -0400

    Vrai ajout de classe Armateur avec les getter et setters.

--- src/modele/Armateur.java
+++ src/modele/Armateur.java
@@ -1,5 +1,23 @@
 package modele;
 
-public class Armateur {
-
+public class Armateur 
+{
+	public int idArmateur;
+	public String nom;
+	
+	
+	public int getIdArmateur() {
+		return idArmateur;
+	}
+	public void setIdArmateur(int idArmateur) {
+		this.idArmateur = idArmateur;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	
 }
